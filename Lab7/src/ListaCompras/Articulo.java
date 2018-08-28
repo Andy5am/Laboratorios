0a3d67aaaff6f33371b5f179e721d69369695345
Andy5am <38666859+Andy5am@users.noreply.github.com>
2018-08-27 22:41:39 -0600

    clases

---
+++ Lab7/src/ListaCompras/Articulo.java
@@ -0,0 +1,31 @@
+package ListaCompras;
+
+import javafx.beans.property.SimpleFloatProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Articulo {
+    SimpleStringProperty nombre;
+    SimpleIntegerProperty cantidad;
+    SimpleFloatProperty precio;
+    SimpleFloatProperty total;
+    SimpleStringProperty estado = new SimpleStringProperty("Pendiente");
+
+    public Articulo(String nombre, int cantidad, float precio){
+        this.nombre = new SimpleStringProperty(nombre);
+        this.cantidad = new SimpleIntegerProperty(cantidad);
+        this.precio = new SimpleFloatProperty(precio);
+        this.total = new SimpleFloatProperty(cantidad*precio);
+    }
+
+    public void cambiarEstado(){
+        if (this.estado.equals("Pendiente")){
+            this.estado = new SimpleStringProperty("Comprado");
+        }else{
+            this.estado= new SimpleStringProperty("Pendiente");
+        }
+    }
+
+}
+
+
---
+++ Lab7/src/ListaCompras/ListaCompras.java
@@ -0,0 +1,10 @@
+package ListaCompras;
+
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class ListaCompras {
+    SimpleStringProperty nombre;
+    SimpleListProperty<Articulo> articulos = new SimpleListProperty();
+
+}
